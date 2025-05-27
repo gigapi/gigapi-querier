319df471740dd05d1b1ec766288e21b86a93ac6d
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2025-05-27 13:42:34 +0200

    Update queryClient.go

--- querier/queryClient.go
+++ querier/queryClient.go
@@ -18,7 +18,7 @@ import (
 	_ "github.com/marcboeker/go-duckdb/v2"
 	"github.com/pingcap/tidb/pkg/parser"
 	"github.com/pingcap/tidb/pkg/parser/ast"
-	_ "github.com/pingcap/tidb/pkg/parser/test_driver"
+	"github.com/pingcap/tidb/pkg/parser/test_driver"
 	"github.com/pingcap/tidb/pkg/parser/opcode"
 )
 
